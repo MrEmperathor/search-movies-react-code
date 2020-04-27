d91e6f943901c070a4443dbd6a1342804cf090da
mremperathor <djalberthvps10@gmail.com>
2020-04-26 20:19:55 -0500

    Updates

---
+++ precache-manifest.bd63592f9d7f8e20e33dce469f96d575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc1950a168e6b62d5e9997119613221",
+    "url": "/search-movies-react/index.html"
+  },
+  {
+    "revision": "c587b5d0fef519486a98",
+    "url": "/search-movies-react/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "5fb2bf5a638c6d558666",
+    "url": "/search-movies-react/static/css/main.165ee4ad.chunk.css"
+  },
+  {
+    "revision": "c587b5d0fef519486a98",
+    "url": "/search-movies-react/static/js/2.7c6ec08c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-movies-react/static/js/2.7c6ec08c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb2bf5a638c6d558666",
+    "url": "/search-movies-react/static/js/main.205398dc.chunk.js"
+  },
+  {
+    "revision": "eba0bfb0a710cd1a1009",
+    "url": "/search-movies-react/static/js/runtime-main.e005646f.js"
+  }
+]);
